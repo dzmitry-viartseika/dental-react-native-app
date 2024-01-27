27c106a8cb74e0a9f4c57b5919f5b61d31e5e5d6
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-01-27 23:29:45 +0100

    add eslint & prettier

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+import { PatientList } from './components/Patient';
+import { patients } from './constants/patients';
+import { React } from 'react';
+
+const App = () => {
+  return (
+    <Container>
+      <ScrollViewLayout>
+        <PatientList title="16th of September" items={patients} />
+        <PatientList title="17th of September" items={patients} />
+      </ScrollViewLayout>
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  flex: 1;
+  margin-top: 50px;
+`;
+
+const ScrollViewLayout = styled.ScrollView`
+  marginhorizontal: 20;
+`;
+
+export default App;
