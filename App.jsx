c2fcdd2b8c5da3a6ef1b430de9e641420a3a4791
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-01-28 19:06:24 +0100

    add plus button

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 import { patients } from './constants/patients';
 import PatientItem from './components/Patient/PatientItem';
 import SectionTitle from './components/Patient/SectionTitle';
+import { Ionicons } from '@expo/vector-icons';
 import { React } from 'react';
 import { SectionList, SafeAreaView } from 'react-native';
 
@@ -10,14 +11,17 @@ const App = () => {
     <Container>
       <SafeAreaView>
         <SectionList
-          sections={patients}
-          keyExtractor={(item, index) => item + index}
-          renderItem={({ item }) => <PatientItem item={item} />}
-          renderSectionHeader={({ section: { title } }) => (
-            <SectionTitle title={title} />
-          )}
+                  sections={patients}
+                  keyExtractor={(item, index) => item + index}
+                  renderItem={({ item }) => <MainSection><PatientItem item={item} /></MainSection> }
+                  renderSectionHeader={({ section: { title } }) => (
+                    <MainSection><SectionTitle title={title} /></MainSection>
+                  )}
         />
       </SafeAreaView>
+      <PlusButton>
+        <Ionicons name="add-outline" size={18} color="white" />
+      </PlusButton>
     </Container>
   );
 };
@@ -26,6 +30,22 @@ const Container = styled.View`
   flex: 1;
   margin-top: 50px;
 `;
+
+const MainSection = styled.View`
+    padding: 10px;
+`
+
+const PlusButton = styled.TouchableOpacity`
+    border-radius: 50px;
+    width: 64px;
+    height: 64px;
+    background: #2A86FF;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    bottom: 25px;
+    right: 15px;
+`
 
 App.displayName = 'App';
 export default App;
