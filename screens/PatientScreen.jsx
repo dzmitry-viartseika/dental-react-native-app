96e750b25a5c665bcac1d968b7e24280b78e4d59
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-01-28 20:50:43 +0100

    add patient card

--- screens/PatientScreen.jsx
+++ screens/PatientScreen.jsx
@@ -1,13 +1,24 @@
 import styled from 'styled-components/native';
 import { React } from 'react';
-import { Text } from 'react-native';
+import { Text, Button } from 'react-native';
 
 const PatientScreen = ({ route }) => {
   const { patient } = route.params;
 
+  const onPressLearnMore = () => {
+    console.log('onPressLearnMore')
+  }
+
   return (
     <Container>
         <Text>{patient.userInfo.fullName}</Text>
+        <PhoneNumber>{patient.userInfo.phoneNumber}</PhoneNumber>
+        <Button
+          onPress={onPressLearnMore}
+          title="Формула зубов"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
     </Container>
   );
 };
@@ -16,6 +27,11 @@ const Container = styled.View`
   flex: 1;
   margin-top: 50px;
 `;
+
+const PhoneNumber = styled.Text`
+    font-size: 20px;
+    color: 'grey'
+`
 
 PatientScreen.displayName = 'PatientScreen';
 export default PatientScreen;
