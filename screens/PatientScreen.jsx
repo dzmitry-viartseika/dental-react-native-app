213732328bb3eee9a749b8985bab6cd4e0cf543d
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-02-03 11:41:34 +0100

    fix pipeline and add node v20

--- screens/PatientScreen.jsx
+++ screens/PatientScreen.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 import { React } from 'react';
-import { Text, Button } from 'react-native';
+import { Button } from 'react-native';
 
 const PatientScreen = ({ route }) => {
   const { patient } = route.params;
@@ -26,17 +26,18 @@ const PatientScreen = ({ route }) => {
 const Container = styled.View`
   flex: 1;
   margin-top: 50px;
+  pading: 0 10px;
 `;
 
 const PatientFullName = styled.Text`
-    font-weight: 800;
-    font-size: 28px;
-    line-height: 30px;
+  font-weight: 800;
+  font-size: 28px;
+  line-height: 30px;
 `;
 
 const PhoneNumber = styled.Text`
-  font-size: 20px;
-  color: 'grey';
+  font-size: 14px;
+  color: grey;
 `;
 
 PatientScreen.displayName = 'PatientScreen';
