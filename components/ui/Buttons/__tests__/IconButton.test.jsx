bd4f51b1dc78710b70735902cdef7b1b5f2707e8
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-02-04 17:06:57 +0100

    add icon component

--- components/ui/Buttons/__tests__/IconButton.test.jsx
+++ components/ui/Buttons/__tests__/IconButton.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react-native';
-import { IconButton } from '../IconButton';
+// import { IconButton } from '../IconButton';
 
 const STUB_COLOR = 'black';
 const STUB_ICON_NAME = 'call';
