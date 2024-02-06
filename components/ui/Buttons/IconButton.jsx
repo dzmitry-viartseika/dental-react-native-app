8f55814f1bfd62b19fb95b5222e8eb96e9a3b4d5
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-02-06 21:22:02 +0100

    add patient visit info

--- components/ui/Buttons/IconButton.jsx
+++ components/ui/Buttons/IconButton.jsx
@@ -3,9 +3,10 @@ import { React } from 'react';
 import PropTypes from 'prop-types';
 import { Ionicons } from '@expo/vector-icons';
 
-const IconButton = ({ iconName, size = 18, color }) => {
+const IconButton = ({ iconName, size = 18, color, background }) => {
+    console.log('background', background);
 	return (
-		<CustomIconButton>
+		<CustomIconButton background={background}>
 			<Ionicons name={iconName} size={size} color={color} />
 		</CustomIconButton>
 	);
@@ -16,7 +17,7 @@ const CustomIconButton = styled.TouchableOpacity`
 	justify-content: center;
 	align-items: center;
 	border-radius: 30px;
-	background: green;
+	background: ${(props) => (props.background ? props.background : '#4294ff')};
 	height: 45px;
 `;
 
@@ -25,5 +26,6 @@ IconButton.propTypes = {
 	iconName: PropTypes.string.isRequired,
 	size: PropTypes.string.isRequired,
 	color: PropTypes.string.isRequired,
+	background: PropTypes.string,
 };
 export default IconButton;
--- screens/PatientScreen.jsx
+++ screens/PatientScreen.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 import { React } from 'react';
 import ButtonComponent from '../components/ui/Buttons/ButtonComponent';
 import IconButton from '../components/ui/Buttons/IconButton';
+import { View, Text } from 'react-native';
 
 const PatientScreen = ({ route }) => {
 	const { patient } = route.params;
@@ -12,29 +13,65 @@ const PatientScreen = ({ route }) => {
 
 	return (
 		<Container>
-			<PatientFullName>{patient.userInfo.fullName}</PatientFullName>
-			<PhoneNumber>{patient.userInfo.phoneNumber}</PhoneNumber>
-			<ActionWrapper>
-				<Test>
-					<ButtonComponent
-						onPressEventHandler={onPressLearnMore}
-						title="Формула зубов"
-						color="#4294ff"
-						accessibilityLabel="Learn more about this purple button"
-					/>
-				</Test>
-				<Test2>
-					<IconButton iconName="call" size={18} color="white" />
-				</Test2>
-			</ActionWrapper>
+		    <MainSection>
+		        <PatientFullName>{patient.userInfo.fullName}</PatientFullName>
+                			<PhoneNumber>{patient.userInfo.phoneNumber}</PhoneNumber>
+                			<ActionWrapper>
+                				<Test>
+                					<ButtonComponent
+                						onPressEventHandler={onPressLearnMore}
+                						title="Формула зубов"
+                						color="#4294ff"
+                						accessibilityLabel="Learn more about this purple button"
+                					/>
+                				</Test>
+                				<Test2>
+                					<IconButton iconName="call" size={18} color="white" />
+                				</Test2>
+                			</ActionWrapper>
+		    </MainSection>
+		    <VisitSection>
+		    			<PatientVisits>
+            			    <ActionWrapper>
+            			        <IconButton iconName="call" size={18} color="gray" background="white" /><Text>Зуб: 12 </Text>
+            			    </ActionWrapper>
+            			    <ActionWrapper>
+            			        <IconButton iconName="call" size={18} color="gray" background="white" /><Text>Диагноз: пульпит</Text>
+            			    </ActionWrapper>
+                            <DatePricePanel>
+                                <Text>11.10.2024 - 15:40</Text>
+                                <Text>1500 P</Text>
+                            </DatePricePanel>
+            			</PatientVisits>
+		    </VisitSection>
 		</Container>
 	);
 };
 
+const PatientVisits = styled.View`
+    padding: 10px;
+    border-radius: 10px;
+    background: white;
+`
+
+const MainSection = styled.View`
+    background: white;
+    	padding: 50px 20px;
+`
+
+const VisitSection = styled.View`
+    padding: 30px 20px;
+`
+
+const DatePricePanel = styled.View`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+`
+
 const Container = styled.View`
 	flex: 1;
-	margin-top: 50px;
-	padding: 0 20px;
 `;
 
 const ActionWrapper = styled.View`
